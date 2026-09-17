2c15b8b2ae3f4c26ad9e0e17b7052eb630d8667d
wonnowone <harmonace10@gmail.com>
2026-09-17 15:44:44 +0900

    birds

--- src/main/java/birds/Penguin.java
+++ src/main/java/birds/Penguin.java
@@ -12,9 +12,9 @@ public class Penguin implements Bird {
         this.numberOfFeathers -= 1;
     }
 
-    public void fly() {
-        throw new UnsupportedOperationException();
-    }
+//    public void fly() {
+//        throw new UnsupportedOperationException();
+//    }
 
     public void swim() {
         this.currentLocation = "in the water";
